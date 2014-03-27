14ca47d340dc22908bbd22acbaa5a94554064221
Akhilesh <akhile@dr.com>
2014-03-27 14:30:31 +0400

    Updates

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 exports.routes = function (map) {
-    map.resources('posts');
-    map.get('welcome', 'welcome#index', {as: 'welcome'});
     map.get('/', 'welcome#index', {as: 'root'});
+    map.get('welcome', 'welcome#index', {as: 'welcome'});
+    map.resources('posts');
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
     map.all(':controller/:action');
