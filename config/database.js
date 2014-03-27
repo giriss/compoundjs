fd08e460e3d5b1e07c5e14eb04a88fe7731d6d71
Akhilesh <akhile@dr.com>
2014-03-27 23:45:09 +0400

    Updates

--- config/database.js
+++ config/database.js
@@ -6,7 +6,12 @@ module.exports =
     { "driver":   "memory"
     }
   , "production":
-    { driver:   "mongodb"
-    , url:      "mongodb://admin:Neha1803@oceanic.mongohq.com:10002/app23431960"
+    {
+      driver:   "redis"
+    , host:     "pub-redis-13848.us-east-1-3.3.ec2.garantiadata.com"
+    , port:     13848
+    , password: "wV2xV3z8IolVrla5"
+//     driver:   "mongodb"
+//    , url:      "mongodb://admin:Neha1803@oceanic.mongohq.com:10002/app23431960"
     }
   };
